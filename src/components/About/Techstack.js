6d5197785017214a40db517a09ece60ccb8f69de
G-Wang12 <gordonwang0608@gmail.com>
2025-04-18 01:29:39 -0400

    updated tech stack section

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,71 +1,76 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Container, OverlayTrigger, Tooltip } from "react-bootstrap";
+import {
+  DiJavascript1, DiReact, DiNodejs, DiPython, DiGit, DiJava
+} from "react-icons/di";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-  DiAngularSimple,
-} from "react-icons/di";
-import {
-  SiFirebase,
-  SiPostgresql,
-  SiSpringboot,
-  SiPandas,
+  SiTypescript, SiSpringboot, SiPostgresql, SiMysql, SiRuby,
+  SiRubyonrails, SiGraphql, SiRedis, SiExpress, SiPandas,
+  SiNumpy, SiHtml5, SiCss3, SiFlask,
 } from "react-icons/si";
+
+const Section = ({ title, children }) => (
+  <>
+    <h4 style={{
+      textAlign: "center",
+      margin: "40px 0 20px",
+      fontWeight: 600,
+    }}>
+      {title}
+    </h4>
+    <Row style={{ justifyContent: "center", paddingBottom: "20px" }}>
+      {children}
+    </Row>
+  </>
+);
+
+const Icon = ({ title, children }) => (
+  <Col xs={4} md={2} className="tech-icons">
+    <OverlayTrigger
+      placement="top"
+      overlay={<Tooltip id={`tooltip-${title}`}>{title}</Tooltip>}
+    >
+      <div style={{ display: "inline-block" }}>{children}</div>
+    </OverlayTrigger>
+  </Col>
+);
 
 function Techstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* Programming Languages */}
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
+    <Container>
+      <Section title="Languages">
+        <Icon title="Java"><DiJava /></Icon>
+        <Icon title="Python"><DiPython /></Icon>
+        <Icon title="JavaScript"><DiJavascript1 /></Icon>
+        <Icon title="TypeScript"><SiTypescript /></Icon>
+        <Icon title="C++"><CgCPlusPlus /></Icon>
+        <Icon title="Ruby"><SiRuby /></Icon>
+        <Icon title="GraphQL"><SiGraphql /></Icon>
+        <Icon title="HTML"><SiHtml5 /></Icon>
+        <Icon title="CSS"><SiCss3 /></Icon>
+      </Section>
 
-      {/* Libraries */}
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPandas />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
+      <Section title="Frameworks & Libraries">
+        <Icon title="React"><DiReact /></Icon>
+        <Icon title="Node.js"><DiNodejs /></Icon>
+        <Icon title="Spring Boot"><SiSpringboot /></Icon>
+        <Icon title="Ruby on Rails"><SiRubyonrails /></Icon>
+        <Icon title="Express.js"><SiExpress /></Icon>
+        <Icon title="Flask"><SiFlask /></Icon>
+      </Section>
 
-      {/* Frameworks */}
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSpringboot />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiAngularSimple />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-    </Row>
+      <Section title="Databases">
+        <Icon title="PostgreSQL"><SiPostgresql /></Icon>
+        <Icon title="MySQL"><SiMysql /></Icon>
+        <Icon title="Redis"><SiRedis /></Icon>
+      </Section>
+
+      <Section title="Data Science">
+        <Icon title="Pandas"><SiPandas /></Icon>
+        <Icon title="NumPy"><SiNumpy /></Icon>
+      </Section>
+    </Container>
   );
 }
 
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,44 +1,67 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Container, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { DiGit } from "react-icons/di";
 import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiIntellijidea,
-  SiClickup,
-  SiMicrosoftteams,
-  SiGithub,
-  SiDocker
+  SiVisualstudiocode, SiPostman, SiSlack, SiIntellijidea,
+  SiClickup, SiMicrosoftteams, SiGithub, SiDocker, SiFigma,
+  SiAmazonaws, SiJupyter, SiTableau, SiLinux,
 } from "react-icons/si";
+
+const Section = ({ title, children }) => (
+  <>
+    <h4 style={{
+      textAlign: "center",
+      margin: "40px 0 20px",
+      fontWeight: 600,
+    }}>
+      {title}
+    </h4>
+    <Row style={{ justifyContent: "center", paddingBottom: "20px" }}>
+      {children}
+    </Row>
+  </>
+);
+
+const Icon = ({ title, children }) => (
+  <Col xs={4} md={2} className="tech-icons">
+    <OverlayTrigger
+      placement="top"
+      overlay={<Tooltip id={`tooltip-${title}`}>{title}</Tooltip>}
+    >
+      <div style={{ display: "inline-block" }}>{children}</div>
+    </OverlayTrigger>
+  </Col>
+);
 
 function Toolstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiIntellijidea />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDocker />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMicrosoftteams />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiClickup />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGithub />
-      </Col>
-    </Row>
+    <Container>
+      <Section title="IDEs & Code Editors">
+        <Icon title="IntelliJ IDEA"><SiIntellijidea /></Icon>
+        <Icon title="VS Code"><SiVisualstudiocode /></Icon>
+        <Icon title="Jupyter Notebook"><SiJupyter /></Icon>
+      </Section>
+
+      <Section title="Version Control & DevOps">
+        <Icon title="Git"><DiGit /></Icon>
+        <Icon title="GitHub"><SiGithub /></Icon>
+        <Icon title="Docker"><SiDocker /></Icon>
+        <Icon title="AWS"><SiAmazonaws /></Icon>
+        <Icon title="Linux"><SiLinux /></Icon>
+      </Section>
+
+      <Section title="Design & Visualization">
+        <Icon title="Figma"><SiFigma /></Icon>
+        <Icon title="Tableau"><SiTableau /></Icon>
+      </Section>
+
+      <Section title="Testing & Collaboration Tools">
+        <Icon title="Postman"><SiPostman /></Icon>
+        <Icon title="Slack"><SiSlack /></Icon>
+        <Icon title="Microsoft Teams"><SiMicrosoftteams /></Icon>
+        <Icon title="ClickUp"><SiClickup /></Icon>
+      </Section>
+    </Container>
   );
 }
 
